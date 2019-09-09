8f5ebb07dbc9c8c8096490ef9cf344dbdc02df57
Marcos Vinicius de Oliveira <marcoskira@users.noreply.github.com>
2019-09-09 00:15:47 -0300

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,18 @@
+class Pixel:
+    r = 0
+    g = 0
+    b = 0
+    bgr = [0, 0, 0]
+
+    def __init__(self, x, y, label):
+        self.x = x
+        self.y = y
+        self.label = label
+
+
+class Blob:
+
+    def __init__ (self):
+        self.roi = []
+        self.pixels_list = []
+        self.pixels_qty = 0
